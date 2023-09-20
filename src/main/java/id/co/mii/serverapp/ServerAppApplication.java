043840a3ba49877e05015d1b50038bccf3afcb4e
romegaarini <romega.purba123@gmail.com>
2023-09-21 06:33:46 +0700

    Message

---
+++ src/main/java/id/co/mii/serverapp/ServerAppApplication.java
@@ -0,0 +1,19 @@
+package id.co.mii.serverapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerAppApplication.class, args);
+
+		System.out.println("\nServer App is running ... \n");
+		
+		// System.out.println("kucing");
+	}
+
+}
+
+
