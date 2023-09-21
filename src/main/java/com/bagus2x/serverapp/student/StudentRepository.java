248eb3f9ea8360110b2a84e57507c1dd0e257438
Tubagus Saifulloh <dutapanggulu@gmail.com>
2023-09-22 06:09:49 +0700

    add student entity, controller, and repository test.

---
+++ src/main/java/com/bagus2x/serverapp/student/StudentRepository.java
@@ -0,0 +1,8 @@
+package com.bagus2x.serverapp.student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+}
