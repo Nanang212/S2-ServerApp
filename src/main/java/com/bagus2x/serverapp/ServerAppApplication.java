6daacf8581983b0d2056510b96cee4960c2611dd
Tubagus Saifulloh <dutapanggulu@gmail.com>
2023-09-22 06:00:12 +0700

    remove unused bean

--- src/main/java/com/bagus2x/serverapp/ServerAppApplication.java
+++ src/main/java/com/bagus2x/serverapp/ServerAppApplication.java
@@ -2,14 +2,12 @@ package com.bagus2x.serverapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ServerAppApplication {
 
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(ServerAppApplication.class, args);
-		HelloController bean = context.getBean(HelloController.class);
-		System.out.println("\nServer App is running...\n" + bean);
+        SpringApplication.run(ServerAppApplication.class, args);
+        System.out.println("\nServer App is running...\n");
     }
 }
