5826f0019ad55d978ca9f557e7d14a941ddaf971
edoergi <yogoergi@gmail.com>
2023-09-21 16:24:56 +0700

    inisialisasi awal

---
+++ src/main/java/id/co/mii/serverapp/ServerAppApplication.java
@@ -0,0 +1,15 @@
+package id.co.mii.serverapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerAppApplication.class, args);
+
+		System.out.println("\nServer App is running ...\n");
+	}
+
+}
