99094049bc4cd70525e106101ecc07ac6fb59283
amarikhbat <amarikhbat@gmail.com>
2023-09-22 16:50:43 +0700

    adding CRUD

---
+++ src/main/java/id/co/mii/serverapp/repositories/RegionRepository.java
@@ -0,0 +1,10 @@
+package id.co.mii.serverapp.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import id.co.mii.serverapp.models.Region;
+
+public interface RegionRepository extends JpaRepository<Region, Integer>{
+    
+}
---
+++ src/main/java/id/co/mii/serverapp/services/RegionService.java
@@ -0,0 +1,56 @@
+package id.co.mii.serverapp.services;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import id.co.mii.serverapp.models.Region;
+import id.co.mii.serverapp.repositories.RegionRepository;
+
+@Service
+public class RegionService {
+    private RegionRepository regionRepository;
+
+
+    
+    public RegionService(RegionRepository regionRepository) {
+        this.regionRepository = regionRepository;
+    }
+
+
+
+    public List<Region> getAll(){
+        return regionRepository.findAll();
+    }
+
+
+    public Region getById(Integer id){
+        return regionRepository.findById(id).orElseThrow(() -> new ResponseStatusException
+        (HttpStatus.NOT_FOUND, "Region not found!"));
+    }
+
+    // create 
+    public Region create(Region region){
+        return regionRepository.save(region);
+    }
+
+    public Region update(Integer id,Region region){
+        // find id
+        getById(id);
+        // set data
+        region.setId(id);
+        // save
+        return regionRepository.save(region);        
+    }
+
+
+    public Region delete(Integer id){
+        Region region = getById(id);
+        regionRepository.delete(region);
+        return region;
+    }
+
+
+}
