88b16a8e5bca2d9ea2d8d4b82bbeddd990742ebf
fuzi <fzein.1695@gmail.com>
2023-09-22 15:43:04 +0700

    crud region

---
+++ src/main/java/id/co/mii/serverapp/services/RegionService.java
@@ -0,0 +1,46 @@
+package id.co.mii.serverapp.services;
+
+import id.co.mii.serverapp.models.Region;
+import id.co.mii.serverapp.repositories.RegionRepository;
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+public class RegionService {
+
+  private RegionRepository regionRepository;
+
+  public RegionService(RegionRepository regionRepository) {
+    this.regionRepository = regionRepository;
+  }
+
+  public List<Region> getAll() {
+    return regionRepository.findAll();
+  }
+
+  public Region getById(Integer id) {
+    return regionRepository
+      .findById(id)
+      .orElseThrow(() ->
+        new ResponseStatusException(HttpStatus.NOT_FOUND, "Region not found!!!")
+      );
+  }
+
+  public Region create(Region region) {
+    return regionRepository.save(region);
+  }
+
+  public Region update(Integer id, Region region) {
+    getById(id);
+    region.setId(id);
+    return regionRepository.save(region);
+  }
+
+  public Region delete(Integer id) {
+    Region region = getById(id);
+    regionRepository.delete(region);
+    return region;
+  }
+}
