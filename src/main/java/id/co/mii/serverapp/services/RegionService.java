7d2f7e55200546a9de86c110fea5b80f029b107a
nanang <nanangaditya2001@gmail.com>
2023-09-22 15:46:02 +0700

    Menambahkan CRUD

---
+++ src/main/java/id/co/mii/serverapp/services/RegionService.java
@@ -0,0 +1,52 @@
+package id.co.mii.serverapp.services;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import id.co.mii.serverapp.models.Region;
+import id.co.mii.serverapp.repositories.RegionRepository;
+
+@Service
+public class RegionService {
+    private RegionRepository regionRepository;
+
+    public RegionService(RegionRepository regionRepository) {
+        this.regionRepository = regionRepository;
+    }
+
+    public List<Region> getAll() {
+        return regionRepository.findAll();
+    }
+
+    public Region getById(Integer id) {
+        return regionRepository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "tidak ditemukan id no :" + id));
+    }
+
+    public Region insertData(Region region) {
+        if (region == null || region.getName() == null || region.getName().isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Nama Region Harus Diisi");
+        }
+
+        try {
+            return regionRepository.save(region);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Gagal menyimpan Region.");
+        }
+    }
+
+    public Region update(Integer id, Region region) {
+        getById(id);
+        region.setId(id);
+        return regionRepository.save(region);
+    }
+
+    public Region Delete(Integer id) {
+        Region region = getById(id);
+        regionRepository.delete(region);
+        return region;
+    }
+}
