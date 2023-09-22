c3009bf81454d84eca85028e004bed314364e1a4
ghofuba <ghofuba@gmail.com>
2023-09-22 23:43:36 +0700

    build (country controller): add
    build country controller to set endpoint of restcontroller country, give annotation @RestController and set request mapping to set endpoint and set every single method to generate crud then giving http method to get data

---
+++ src/main/java/id/co/mii/serverapp/controllers/CountryController.java
@@ -0,0 +1,48 @@
+package id.co.mii.serverapp.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import id.co.mii.serverapp.models.Country;
+import id.co.mii.serverapp.services.CountryService;
+
+@RestController
+@RequestMapping("/country")
+public class CountryController {
+    
+    private CountryService countryService;
+
+    @GetMapping
+    public List<Country> getAll(){
+        return countryService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Country getById(Integer id){
+        return countryService.getById(id);
+    }
+
+    @PostMapping
+    public Country create(@RequestBody Country country){
+        return countryService.create(country);
+    }
+
+    @PutMapping("/{id}")
+    public Country update(@RequestBody @PathVariable Country country, Integer id){
+        return countryService.update(country, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public Country delete(@PathVariable Integer id){
+        return countryService.delete(id);
+    }
+
+}
