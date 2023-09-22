b2660cb6df484c45f12229edd68e7cbcf16466bb
ghofuba <ghofuba@gmail.com>
2023-09-22 23:11:06 +0700

    refactor (entity regionrelationship): change
    change relationship region which is have one to many relationship with countries, add annotation @OneToMany mappedBy to indicite that relationship is owned by the other entity is country entity

--- src/main/java/id/co/mii/serverapp/models/Region.java
+++ src/main/java/id/co/mii/serverapp/models/Region.java
@@ -1,15 +1,21 @@
 package id.co.mii.serverapp.models;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+
 
 @Entity
 @Table(name = "tb_region")
 public class Region {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) 
     @Column(name = "region_id")
@@ -18,14 +24,16 @@ public class Region {
     @Column(name = "region_name", nullable = false, length = 20)
     private String name;
 
+    @OneToMany(mappedBy = "country")
+    private List<Country> countries;
 
-    
     public Region() {
     }
 
-    public Region(Integer id, String name) {
+    public Region(Integer id, String name, List<Country> countries) {
         this.id = id;
         this.name = name;
+        this.countries = countries;
     }
 
     public Integer getId() {
@@ -44,12 +52,12 @@ public class Region {
         this.name = name;
     }
 
-    @Override
-    public String toString() {
-        return "Region [name=" + name + "]";
+    public List<Country> getCountries() {
+        return countries;
     }
 
-     
-    
+    public void setCountries(List<Country> countries) {
+        this.countries = countries;
+    }
 
 }
