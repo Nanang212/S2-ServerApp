79b5e95914cfd99d6c35b3044496923196545997
fuzi <fzein.1695@gmail.com>
2023-09-27 14:22:45 +0700

    one to many - region to country

--- src/main/java/id/co/mii/serverapp/models/Country.java
+++ src/main/java/id/co/mii/serverapp/models/Country.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -27,4 +29,8 @@ public class Country {
 
   @Column(name = "country_name", nullable = false, length = 20)
   private String name;
+
+  @ManyToOne
+  @JoinColumn(name = "region_id", nullable = false)
+  private Region region;
 }
--- src/main/java/id/co/mii/serverapp/models/Region.java
+++ src/main/java/id/co/mii/serverapp/models/Region.java
@@ -1,10 +1,13 @@
 package id.co.mii.serverapp.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -24,4 +27,8 @@ public class Region {
 
   @Column(name = "region_name", nullable = false, length = 20)
   private String name;
+
+  @OneToMany(mappedBy = "region")
+  @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+  private List<Country> countries;
 }
