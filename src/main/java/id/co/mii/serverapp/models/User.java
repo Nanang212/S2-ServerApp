c0e69beb924ebac1522bd33ada069d77d928aa4f
fuzi <fzein.1695@gmail.com>
2023-10-04 13:56:17 +0700

    many to many - user to role

--- src/main/java/id/co/mii/serverapp/models/User.java
+++ src/main/java/id/co/mii/serverapp/models/User.java
@@ -1,9 +1,12 @@
 package id.co.mii.serverapp.models;
 
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -31,4 +34,12 @@ public class User {
   @MapsId
   @JoinColumn(name = "id")
   private Employee employee;
+
+  @ManyToMany
+  @JoinTable(
+    name = "tb_tr_user_role",
+    joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id")
+  )
+  private List<Role> roles;
 }
