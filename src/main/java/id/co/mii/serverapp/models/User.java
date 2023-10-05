2f758cdccf574484a1bffa5c3768f2962ec00938
ghofuba <ghofuba@gmail.com>
2023-10-05 14:18:11 +0700

    match ManyToMany User-Role

--- src/main/java/id/co/mii/serverapp/models/User.java
+++ src/main/java/id/co/mii/serverapp/models/User.java
@@ -1,11 +1,16 @@
 package id.co.mii.serverapp.models;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,6 +20,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "tb_user")
 public class User {
     
     @Id
@@ -30,4 +36,12 @@ public class User {
     @MapsId
     @JoinColumn(name = "id")
     private Employee employee;
+
+    @ManyToMany
+    @JoinTable(
+        name = "tb_tr_user_role",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id")
+     )
+    private List<Role> roles;
 }
