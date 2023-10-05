afe968fba0b5a0ebcbbf9034b9f4877feedad8ba
ghofuba <ghofuba@gmail.com>
2023-10-05 13:48:52 +0700

    match One to one Employee-User

---
+++ src/main/java/id/co/mii/serverapp/models/User.java
@@ -0,0 +1,33 @@
+package id.co.mii.serverapp.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    
+    @Id
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "id")
+    private Employee employee;
+}
