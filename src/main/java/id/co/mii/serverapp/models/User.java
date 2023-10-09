758f0af231d974aecb3b1af7a839d181db9ff890
Tubagus Saifulloh <dutapanggulu@gmail.com>
2023-10-09 13:22:29 +0700

    change nullable constraints

--- src/main/java/id/co/mii/serverapp/models/User.java
+++ src/main/java/id/co/mii/serverapp/models/User.java
@@ -20,12 +20,14 @@ public class User {
     @Id
     @Column(name = "id")
     private Integer id;
-    @Column(name = "username", unique = true, nullable = false)
+    @Column(name = "username", unique = true)
     private String username;
-    @Column(name = "password", nullable = false)
+    @Column(name = "password")
     private String password;
-    @Column(name = "is_enabled", nullable = false)
-    private Boolean isEnabled = true;
+    @Column(name = "is_enabled")
+    private Boolean isEnabled = false;
+    @Column(name = "token")
+    private String token;
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
