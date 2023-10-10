39f949e33a5b371478fe4d5912e17ae8bbaed1f9
Tubagus Saifulloh <dutapanggulu@gmail.com>
2023-10-10 21:41:43 +0700

    remove unused controller and move newsletter sender

--- src/main/java/id/co/mii/serverapp/controllers/EmailController.java
+++ src/main/java/id/co/mii/serverapp/controllers/EmailController.java
@@ -5,10 +5,12 @@ import id.co.mii.serverapp.services.EmailService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @AllArgsConstructor
@@ -42,5 +44,13 @@ public class EmailController {
     )
     public EmailRequest attachment(@RequestBody EmailRequest request) {
         return emailService.sendMessageWithAttachment(request);
+    }
+
+    @GetMapping(
+        value = "/newsletter",
+        produces = MediaType.TEXT_HTML_VALUE
+    )
+    public ModelAndView newsletter() {
+        return new ModelAndView("views/newsletter_form");
     }
 }
