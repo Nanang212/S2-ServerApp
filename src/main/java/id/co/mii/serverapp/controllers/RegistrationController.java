816028b0deac666bf90483e207451f46cd2de78d
nanang <nanangaditya2001@gmail.com>
2023-10-11 10:28:58 +0700

    edit register dan registration

--- src/main/java/id/co/mii/serverapp/controllers/RegistrationController.java
+++ src/main/java/id/co/mii/serverapp/controllers/RegistrationController.java
@@ -52,9 +52,9 @@ public class RegistrationController {
 
     @PostMapping("/adduser")
     public String registration(
-        @ModelAttribute RegistrationRequest registrationRequest) {
+            @ModelAttribute RegistrationRequest registrationRequest) {
         authService.registration(registrationRequest);
-      return "success";
+        return "success";
     }
 
 }
