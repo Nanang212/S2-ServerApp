d1726581d6d659e3946a11bf70c11bc6a5ae7878
Tubagus Saifulloh <dutapanggulu@gmail.com>
2023-10-11 11:04:05 +0700

    add login and configure security

--- src/main/java/id/co/mii/serverapp/controllers/AuthController.java
+++ src/main/java/id/co/mii/serverapp/controllers/AuthController.java
@@ -6,9 +6,11 @@ import id.co.mii.serverapp.models.dto.requests.RegistrationRequest;
 import id.co.mii.serverapp.models.dto.responses.LoginResponse;
 import id.co.mii.serverapp.services.AuthService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @AllArgsConstructor
@@ -23,5 +25,10 @@ public class AuthController {
     @PostMapping("/login")
     public LoginResponse login(@RequestBody LoginRequest request) {
         return authService.login(request);
+    }
+
+    @GetMapping("/login-form")
+    public ModelAndView login() {
+        return new ModelAndView("views/login_form");
     }
 }
