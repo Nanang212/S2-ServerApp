82becaec05f28fd7a3ee50f41f982d70ee8abba1
Tubagus Saifulloh <dutapanggulu@gmail.com>
2023-10-11 10:13:04 +0700

    remove unused method and make it more simple

--- src/main/java/id/co/mii/serverapp/controllers/EmployeeController.java
+++ src/main/java/id/co/mii/serverapp/controllers/EmployeeController.java
@@ -7,9 +7,16 @@ import id.co.mii.serverapp.models.dto.requests.VerifyRequest;
 import id.co.mii.serverapp.services.EmployeeService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDateTime;
@@ -21,38 +28,38 @@ import java.util.List;
 public class EmployeeController {
     private final EmployeeService employeeService;
 
-    @PostMapping(value = "/employee", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseStatus(HttpStatus.CREATED)
-    public Employee create(@RequestBody EmployeeRequest request) {
+    @PostMapping(
+        value = {"/employee", "/employee-registration"},
+        consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Employee create(@RequestBody NewEmployeeRequest request) {
         return employeeService.create(request);
     }
 
-    @GetMapping(value = "/employee/{employeeId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Employee getById(@PathVariable Integer employeeId) {
-        return employeeService.getById(employeeId);
+    @PostMapping(
+        value = "/employee-verification",
+        consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+        produces = MediaType.TEXT_HTML_VALUE)
+    public ModelAndView verify(@ModelAttribute VerifyRequest request
+    ) {
+        ModelAndView modelAndView;
+
+        try {
+            employeeService.verify(request);
+            modelAndView = new ModelAndView("views/employee_verification_success");
+        } catch (Exception exception) {
+            modelAndView = new ModelAndView("redirect:/employee-verification?token=" + request.getToken());
+            modelAndView.addObject("message", exception.getMessage());
+        }
+
+        return modelAndView;
     }
 
-    @GetMapping(value = "/employees", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Employee> getAll() {
-        return employeeService.getAll();
-    }
-
-    @PutMapping(value = "/employee/{employeeId}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Employee update(@PathVariable Integer employeeId, @RequestBody EmployeeRequest request) {
-        return employeeService.update(employeeId, request);
-    }
-
-    @DeleteMapping(value = "/employee/{employeeId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Employee delete(@PathVariable Integer employeeId) {
-        return employeeService.delete(employeeId);
-    }
-
-    @PostMapping(value = "/employee-registration", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Employee register(@RequestBody NewEmployeeRequest request) {
-        return employeeService.register(request);
-    }
-
-    @GetMapping(value = "/employee-verification", produces = MediaType.TEXT_HTML_VALUE)
+    @GetMapping(
+        value = "/employee-verification",
+        produces = MediaType.TEXT_HTML_VALUE
+    )
     public ModelAndView verify(@RequestParam(name = "token") String token) {
         ModelAndView modelAndView;
 
@@ -73,28 +80,52 @@ public class EmployeeController {
         return modelAndView;
     }
 
-    @PostMapping(value = "/employee-verification", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = MediaType.TEXT_HTML_VALUE)
-    public ModelAndView verify(@ModelAttribute VerifyRequest request) {
-        ModelAndView modelAndView;
-
-        try {
-            employeeService.verify(request);
-            modelAndView = new ModelAndView("views/employee_verification_success");
-        } catch (Exception exception) {
-            modelAndView = new ModelAndView("redirect:/employee-verification?token=" + request.getToken());
-            modelAndView.addObject("message", exception.getMessage());
-        }
-
-        return modelAndView;
-    }
-
-    @GetMapping(value = "/employee-dashboard", produces = MediaType.TEXT_HTML_VALUE)
+    @GetMapping(
+        value = "/employee-dashboard",
+        produces = MediaType.TEXT_HTML_VALUE
+    )
     public ModelAndView dashboard() {
         return new ModelAndView("views/employee_dashboard");
     }
 
-    @GetMapping(value = "/employee")
+    @GetMapping(
+        value = "/employee",
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
     public Employee getEmployeeBySession() {
         return employeeService.getEmployeeBySession();
+    }
+
+    @GetMapping(
+        value = "/employee/{employeeId}",
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Employee getById(@PathVariable Integer employeeId) {
+        return employeeService.getById(employeeId);
+    }
+
+    @GetMapping(
+        value = "/employees",
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public List<Employee> getAll() {
+        return employeeService.getAll();
+    }
+
+    @PutMapping(
+        value = "/employee/{employeeId}",
+        consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Employee update(@PathVariable Integer employeeId, @RequestBody EmployeeRequest request) {
+        return employeeService.update(employeeId, request);
+    }
+
+    @DeleteMapping(
+        value = "/employee/{employeeId}",
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Employee delete(@PathVariable Integer employeeId) {
+        return employeeService.delete(employeeId);
     }
 }
--- src/main/java/id/co/mii/serverapp/services/EmployeeService.java
+++ src/main/java/id/co/mii/serverapp/services/EmployeeService.java
@@ -39,78 +39,8 @@ public class EmployeeService {
     private final EmailService emailService;
     private final PasswordEncoder passwordEncoder;
 
-    public Employee create(EmployeeRequest request) {
-        Set<ConstraintViolation<EmployeeRequest>> constraintViolations = validator.validate(request);
-        if (!constraintViolations.isEmpty()) {
-            throw new ConstraintViolationException(constraintViolations);
-        }
-
-        if (employeeRepository.existsByEmailIgnoreCase(request.getEmail())) {
-            throw new ResponseStatusException(HttpStatus.CONFLICT, "Email already exists");
-        }
-
-        if (employeeRepository.existsByUserId(request.getUserId())) {
-            throw new ResponseStatusException(HttpStatus.CONFLICT,
-                    "User with id " + request.getUserId() + " already associate with an employee");
-        }
-
-        Employee employee = new Employee();
-
-        return save(request, employee);
-    }
-
-    public Employee getById(Integer employeeId) {
-        return employeeRepository
-                .findById(employeeId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Employee not found"));
-    }
-
-    public Employee getByToken(String token) {
-        return employeeRepository
-                .findByUserToken(token)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Employee not found"));
-    }
-
-    public List<Employee> getAll() {
-        return employeeRepository.findAll();
-    }
-
-    public Employee update(Integer employeeId, EmployeeRequest request) {
-        Set<ConstraintViolation<EmployeeRequest>> constraintViolations = validator.validate(request);
-        if (!constraintViolations.isEmpty()) {
-            throw new ConstraintViolationException(constraintViolations);
-        }
-
-        Employee employee = getById(employeeId);
-
-        return save(request, employee);
-    }
-
-    private Employee save(EmployeeRequest request, Employee employee) {
-        User user = userRepository
-                .findById(request.getUserId())
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
-
-        employee.setName(request.getName());
-        employee.setEmail(request.getEmail());
-        employee.setPhone(request.getPhone());
-        employee.setUser(user);
-
-        return employeeRepository.save(employee);
-    }
-
-    public Employee delete(Integer employeeId) {
-        Employee employee = employeeRepository
-                .findById(employeeId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Employee not found"));
-
-        employeeRepository.delete(employee);
-
-        return employee;
-    }
-
     @Transactional
-    public Employee register(NewEmployeeRequest request) {
+    public Employee create(NewEmployeeRequest request) {
         Set<ConstraintViolation<NewEmployeeRequest>> constraintViolations = validator.validate(request);
         if (!constraintViolations.isEmpty()) {
             throw new ConstraintViolationException(constraintViolations);
@@ -125,8 +55,8 @@ public class EmployeeService {
         employee.setEmail(request.getEmail());
 
         Role role = roleRepository
-                .findByNameIgnoreCase("USER")
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Role not found"));
+            .findByNameIgnoreCase("USER")
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Role not found"));
 
         User user = new User();
         user.setEmployee(employee);
@@ -138,16 +68,16 @@ public class EmployeeService {
         employee = employeeRepository.save(employee);
 
         emailService.sendMessageWithHtml(
-                new HashMap<String, Object>() {
-                    {
-                        put("token", user.getToken());
-                        put("name", request.getName());
-                    }
-                },
-                "emails/employee_verification",
-                request.getEmail(),
-                "Verify and Update Your Data",
-                null);
+            new HashMap<String, Object>() {
+                {
+                    put("token", user.getToken());
+                    put("name", request.getName());
+                }
+            },
+            "emails/employee_verification",
+            request.getEmail(),
+            "Verify and Update Your Data",
+            null);
 
         return employee;
     }
@@ -183,5 +113,55 @@ public class EmployeeService {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         AppUserDetail appUserDetail = (AppUserDetail) authentication.getPrincipal();
         return appUserDetail.getUser().getEmployee();
+    }
+
+    public Employee getById(Integer employeeId) {
+        return employeeRepository
+            .findById(employeeId)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Employee not found"));
+    }
+
+    public Employee getByToken(String token) {
+        return employeeRepository
+            .findByUserToken(token)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Employee not found"));
+    }
+
+    public List<Employee> getAll() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee update(Integer employeeId, EmployeeRequest request) {
+        Set<ConstraintViolation<EmployeeRequest>> constraintViolations = validator.validate(request);
+        if (!constraintViolations.isEmpty()) {
+            throw new ConstraintViolationException(constraintViolations);
+        }
+
+        Employee employee = getById(employeeId);
+
+        return save(request, employee);
+    }
+
+    private Employee save(EmployeeRequest request, Employee employee) {
+        User user = userRepository
+            .findById(request.getUserId())
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+
+        employee.setName(request.getName());
+        employee.setEmail(request.getEmail());
+        employee.setPhone(request.getPhone());
+        employee.setUser(user);
+
+        return employeeRepository.save(employee);
+    }
+
+    public Employee delete(Integer employeeId) {
+        Employee employee = employeeRepository
+            .findById(employeeId)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Employee not found"));
+
+        employeeRepository.delete(employee);
+
+        return employee;
     }
 }
